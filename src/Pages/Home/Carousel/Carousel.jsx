8b8f1f69683ff0cebd76f188a95d6b0b0bef7878
jfmemon <jannatul15-2497@diu.edu.bd>
2023-09-01 19:29:14 +0600

    carousel design and responsiveness done

--- src/Pages/Home/Carousel/Carousel.jsx
+++ src/Pages/Home/Carousel/Carousel.jsx
@@ -10,61 +10,96 @@ import 'swiper/css/pagination';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 
+import img1 from '../../../assets/Carousel-images/shopByCategory.jpg';
+import img2 from '../../../assets/Carousel-images/sexualWellness.jpg';
+import img3 from '../../../assets/Carousel-images/birthControl.jpg';
+import img4 from '../../../assets/Carousel-images/vitamins.jpg';
+import img5 from '../../../assets/Carousel-images/medicalDevice.jpg';
+import img6 from '../../../assets/Carousel-images/personalCare.jpg';
+import img7 from '../../../assets/Carousel-images/healthAndWellness.jpg';
+import img8 from '../../../assets/Carousel-images/babyCare.jpg';
+
 const Carousel = () => {
     return (
-        <Swiper
-            effect={'coverflow'}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView={'auto'}
-            coverflowEffect={{
-                rotate: 0,
-                stretch: 0,
-                depth: 100,
-                modifier: 2.5,
-            }}
-            pagination={{ el: '.swiper-pagination', clickable: true }}
-            navigation={{
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-                clickable: true,
-            }}
-            modules={[EffectCoverflow, Pagination, Navigation]}
-            className="swiper_container"
-        >
-            <SwiperSlide>
-                <img src={slide_image_1} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide_image_2} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide_image_3} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide_image_4} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide_image_5} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide_image_6} alt="slide_image" />
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide_image_7} alt="slide_image" />
-            </SwiperSlide>
+        <div>
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                loop={true}
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                }}
+                pagination={{ el: '.swiper-pagination', clickable: true }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                    clickable: true,
+                }}
+                modules={[EffectCoverflow, Pagination, Navigation]}
+                className='container my-24 flex flex-col'
+                breakpoints={{
+                    0: {
+                        spaceBetween: 50,
+                        slidesPerView: 1,
+                    },
+                    480: {
+                        spaceBetween: 50,
+                        slidesPerView: 2
+                    },
+                    768: {
+                        spaceBetween: 50,
+                        slidesPerView: 2
+                    },
+                    1024: {
+                        spaceBetween: 50,
+                        slidesPerView: 3
+                    },
+                    1280: {
+                        spaceBetween: 50,
+                        slidesPerView: 3
+                    }
+                }}
+            >
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img1} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img2} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img3} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img4} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img5} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img6} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img7} alt="slide_image" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img className='w-full h-96 rounded-lg' src={img8} alt="slide_image" />
+                </SwiperSlide>
 
-            <div className="slider-controler">
-                <div className="swiper-button-prev slider-arrow">
-                    <ion-icon name="arrow-back-outline"></ion-icon>
+                <div className="slider-controler">
+                    <div className="swiper-button-prev slider-arrow">
+                        <ion-icon name="arrow-back-outline"></ion-icon>
+                    </div>
+                    <div className="swiper-button-next slider-arrow">
+                        <ion-icon name="arrow-forward-outline"></ion-icon>
+                    </div>
+                    <div className="swiper-pagination"></div>
                 </div>
-                <div className="swiper-button-next slider-arrow">
-                    <ion-icon name="arrow-forward-outline"></ion-icon>
-                </div>
-                <div className="swiper-pagination"></div>
-            </div>
-        </Swiper>
+            </Swiper>
+        </div>
     );
 };
 
