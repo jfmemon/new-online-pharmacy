aa7a47ba5358a38db7671e04353c243333cfc5db
unknown <Md.Jannatul Ferdous (Emon)>
2023-10-17 10:39:21 +0600

    user home and admin home button created

--- src/Pages/UserDashboard/UserDashboard.jsx
+++ src/Pages/UserDashboard/UserDashboard.jsx
@@ -14,7 +14,7 @@ const UserDashboard = () => {
 
     return (
         <div className="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle"/>
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col lg:flex-row md:justify-center">
                 {/* Page content here */}
                 <label htmlFor="my-drawer-2" className="drawer-button mr-auto pl-1 lg:hidden"><FontAwesomeIcon icon={faBars} /><span className='ml-1'>Menu</span></label>
@@ -26,8 +26,9 @@ const UserDashboard = () => {
                     {/* Sidebar content here */}
                     <div className='mt-12 lg:mt-[-10px] flex justify-between items-center'>
                         <h3 className='py-2 lg:mt-0 text-2xl text-indigo-500'>User dashboard</h3>
-                        <label className='text-xl text-red-500 font-bold lg:invisible' htmlFor="my-drawer-2" ><FontAwesomeIcon icon={faXmark}/></label>
+                        <label className='text-xl text-red-500 font-bold lg:invisible' htmlFor="my-drawer-2" ><FontAwesomeIcon icon={faXmark} /></label>
                     </div>
+                    <li><NavLink to="/userDashboard/userHome">User home</NavLink></li>
                     <li><NavLink to="/userDashboard/myCart">My cart<span className="badge badge-sm indicator-item text-gray-600 font-bold">{cart?.length}</span></NavLink></li>
                     <li><NavLink to="/userDashboard/orderList">Order list<span className="badge badge-sm indicator-item text-gray-600 font-bold">{orders?.length}</span></NavLink></li>
                     <li><NavLink to="/userDashboard/prescriptions">Prescriptions list<span className="badge badge-sm indicator-item text-gray-600 font-bold">{prescription?.length}</span></NavLink></li>
