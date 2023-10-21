92dc8f472a53ac4a8c6da158928fc5fce71538e1
unknown <Md.Jannatul Ferdous (Emon)>
2023-10-21 20:16:42 +0600

    password showing in allUsers of adminDashboard

--- src/Pages/AdminDashboard/AllUsers.jsx
+++ src/Pages/AdminDashboard/AllUsers.jsx
@@ -70,34 +70,34 @@ const AllUsers = () => {
                     <table className='w-full min-w-max table-auto text-left'>
                         <thead>
                             <tr className='bg-gray-800 text-white'>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>No.</th>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>Image</th>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>Name</th>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>Email</th>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>Password</th>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>Role</th>
-                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-8'>Action</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>No.</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>Image</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>Name</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>Email</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>Password</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>Role</th>
+                                <th className='border-b border-blue-gray-100 bg-blue-gray-50 py-4 px-6'>Action</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
                                 users.map((user, idx) => <tr key={user._id} className='hover:bg-slate-200'>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50'>{idx + 1}</td>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50'>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50'>{idx + 1}</td>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50'>
                                         {
                                             user.photoURL ? <img className='w-10 h-10 rounded-lg' src={user.photoURL} alt="" /> :
                                                 <img className='w-10 h-10 rounded-lg' src={userImage} alt="" />
                                         }
                                     </td>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50'>{user.name}</td>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50 text-center'>{user.email}</td>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50 text-center'>{user.password}</td>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50'>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50'>{user.name}</td>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50 text-center'>{user.email}</td>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50 text-center'>{user.password}</td>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50'>
                                         {
                                             user.role === 'admin' ? 'admin' : <button onClick={() => handleMakeAdmin(user)} className='btn btn-md bg-red-500 text-white text-[15px] hover:text-red-600' title='Make admin!'><FontAwesomeIcon icon={faUserPlus} /></button>
                                         }
                                     </td>
-                                    <td className='py-2 px-8 border-b border-blue-gray-50'>
+                                    <td className='py-2 px-6 border-b border-blue-gray-50'>
                                         <button onClick={() => handleDeleteUser(user)} className='btn btn-md bg-red-500 text-white text-[18px] hover:text-red-600' title='Delete this user!'><FontAwesomeIcon icon={faTrashCan} /></button>
                                     </td>
                                 </tr>)
